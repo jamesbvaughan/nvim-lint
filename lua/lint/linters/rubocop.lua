f8a245a3672f89f842eef2dbb6afe6cf2f1487dc
James Vaughan <jamesbvaughan@stripe.com>
2021-09-22 15:14:52 -0700

    Make the rubocop linter return a function
    This modifies the rubocop linter, making it return a function that
    dynamically determines whether it should call `rubocop` or `bundle exec
    rubocop` rather than a static config.

--- lua/lint/linters/rubocop.lua
+++ lua/lint/linters/rubocop.lua
@@ -17,54 +17,56 @@ local function use_bundler()
   end
 end
 
-return {
-  cmd = function(bufnr)
-    if use_bundler() then
-      return { 'bundle', 'exec', 'rubocop' }
-    else
-      return 'rubocop'
-    end
-  end,
-  stdin = false,
-  args = {
-    '--force-exclusion',
-    '--format', 'json',
-  },
-  ignore_exitcode = true,
-  parser = function(output, bufnr)
-    local items = {}
+return function()
+  local cmd = 'rubocop'
 
-    -- If there is some sort of error, the output stream is empty. Parsing that
-    -- would result in an error when decoding the JSON.
-    if output == '' then
+  if use_bundler() then
+    cmd =  { 'bundle', 'exec', 'rubocop' }
+  end
+
+  return {
+    cmd = cmd,
+    stdin = false,
+    args = {
+      '--force-exclusion',
+      '--format', 'json',
+    },
+    ignore_exitcode = true,
+    parser = function(output, bufnr)
+      local items = {}
+
+      -- If there is some sort of error, the output stream is empty. Parsing that
+      -- would result in an error when decoding the JSON.
+      if output == '' then
+        return items
+      end
+
+      local decoded = vim.fn.json_decode(output) or {}
+
+      for _, file in ipairs(decoded.files) do
+        for _, diag in ipairs(file.offenses) do
+          table.insert(items, {
+            source = 'rubocop',
+            range = {
+              ['start'] = {
+                line = diag.location.start_line - 1,
+                character = diag.location.start_column - 1
+              },
+              ['end'] = {
+                line = diag.location.last_line - 1,
+                character = diag.location.last_column
+              },
+            },
+            message = diag.message,
+            severity = assert(
+              severities[diag.severity],
+              'missing mapping for severity ' .. diag.severity
+            ),
+          })
+        end
+      end
+
       return items
     end
-
-    local decoded = vim.fn.json_decode(output) or {}
-
-    for _, file in ipairs(decoded.files) do
-      for _, diag in ipairs(file.offenses) do
-        table.insert(items, {
-          source = 'rubocop',
-          range = {
-            ['start'] = {
-              line = diag.location.start_line - 1,
-              character = diag.location.start_column - 1
-            },
-            ['end'] = {
-              line = diag.location.last_line - 1,
-              character = diag.location.last_column
-            },
-          },
-          message = diag.message,
-          severity = assert(
-            severities[diag.severity],
-            'missing mapping for severity ' .. diag.severity
-          ),
-        })
-      end
-    end
-
-    return items
-  end
-}
+  }
+end
